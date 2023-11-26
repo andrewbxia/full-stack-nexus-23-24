62b4e80f7cbe48e7c4ead378c422ae54aba3cd91
Andrew Xia <andrewbenxia@gmail.com>
2023-11-26 00:02:27 -0500

    indexing test

--- routes/joinRoutes.js
+++ routes/joinRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("join");
+    res.status(404).send("Sorry, can't find that!");
+    //res.render("join");
 });
 
 module.exports = router;
