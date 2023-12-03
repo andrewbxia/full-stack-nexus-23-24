8719dc5dc548b5c9ca130cb85c1537a1e025d7c5
Andrew Xia <andrewbenxia@gmail.com>
2023-12-03 00:03:52 -0500

    puppeteer collapsed

--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -20,7 +20,13 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage});
 
 async function screenshot(url, outputPath){
-    const browser = await puppeteer.launch({headless:"new"});
+    const browser = await puppeteer.launch({
+        headless:"new",
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+        ],
+    });
     const page = await browser.newPage();
     await page.setViewport({width: 400, height: 300});
     await page.goto(url);
