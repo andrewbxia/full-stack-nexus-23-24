c0e47077f62eb6513311c574f4159b2234fbd45c
Andrew Xia <andrewbenxia@gmail.com>
2023-12-13 21:22:21 -0500

    metadata redo

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const {handleLogin, dblogin} = require("../serverscripts/loginUser.js");
+const BASE_URL = require("../BASE_URL.js");
 
 router.get("/" , (req, res) => {
-    res.render("login", {redirect: req.query.redirect, message: req.query.message, error: req.query.error});
+    res.render("login", {redirect: req.query.redirect, message: req.query.message, error: req.query.error, BASE_URL: BASE_URL});
     console.log(req.session.user);
 });
 
