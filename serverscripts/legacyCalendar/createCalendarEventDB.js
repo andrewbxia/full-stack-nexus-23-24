24049e106f8eb3124d25332a95a52e17885b73ba
Andrew Xia <andrewbenxia@gmail.com>
2024-01-02 01:52:23 -0500

    woops

--- serverscripts/legacyCalendar/createCalendarEventDB.js
+++ serverscripts/legacyCalendar/createCalendarEventDB.js
@@ -1,7 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 
 
-const db = new sqlite3.Database("../fsnDB/legacyCalendarEvents.db", (err) => {
+const db = new sqlite3.Database("../fsnDB/legacyCalendarEvents.db",sqlite3.OPEN_READWRITE, (err) => {
     if(err){
         return console.error(err.message);
     }
