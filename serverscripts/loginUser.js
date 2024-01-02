143badd8b0ec0a020ab5b86c47e45756994d1108
Andrew Xia <andrewbenxia@gmail.com>
2024-01-02 02:32:58 -0500

    omg why was the db file write protected I JUST RAN TOUCH CALENDAREVNTS.DB AUGHHHHHHHHHHH THAT WAS SO MUCH TIME WASTED IN VIM I SAWERATO GOD AAAAAAAAASDJAI

--- serverscripts/loginUser.js
+++ serverscripts/loginUser.js
@@ -13,7 +13,6 @@ const dblogin = new sqlite3.Database("../fsnDB/users.db", sqlite3.OPEN_READWRITE
 });
 
 dblogin.serialize(async() => {
-    console.log(";lskdjf;lskdfj;lskdfj")
     dblogin.run(`CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         mode TEXT NOT NULL,
