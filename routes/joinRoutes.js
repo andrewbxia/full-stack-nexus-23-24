81b33c0292f8b87a27a48c8b9cee144e99cf5de8
Andrew Xia <andrewbenxia@gmail.com>
2024-02-01 02:05:19 -0500

    join polishing, no more 404

--- routes/joinRoutes.js
+++ routes/joinRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(404).render("join");
+    res.render("join");
     console.log("join");
 });
 
